331b494654253d5ecc73a5da48640acbc4eb1a44
RLynx0 <luc.signin96@gmail.com>
2025-02-05 00:02:18 +0100

    Made gammashift even better

--- .config/scripts/gammashift.sh
+++ .config/scripts/gammashift.sh
@@ -1,20 +1,27 @@
 #!/usr/bin/bash
 
+STATE_HOME="${XDG_DATA_HOME:-"$HOME/.local/share"}/gammashift"
 PRECISION="3"
 property="$1"
 input_val="$2"
 time="${3:-"2"}"
 timestep="${4:-"0.025"}"
 
+[ "$input_val" = "undo" ] && target="$(cat "$STATE_HOME/$property.prev")"
 [[ "$input_val" =~ ^([+*/-]?)\s*([0-9]+(\.[0-9]+)?)$ ]] \
 && op="${BASH_REMATCH[1]}" && target="${BASH_REMATCH[2]}"
+[ -z "$target" ] && exit 1
+
 val="$(busctl --user -- get-property rs.wl-gammarelay / rs.wl.gammarelay "$property")"
-[ -z "$target" ] && exit 1; char="${val% *}"; val="${val#* }"
+char="${val% *}"; val="${val#* }"
+[ -d "$STATE_HOME" ] || mkdir "$STATE_HOME"
+echo "$val" > "$STATE_HOME/$property.prev"
+
 [ -z "$op" ] || target="$(calc -p "round($val $op $target, $PRECISION)")"
 d="$(calc -p "round(($target - $val) * $timestep / $time, $PRECISION)")"
-echo "$val -> $target (${time}s)" > /dev/stderr
+echo "$property : $val -> $target (${time}s)" > /dev/stderr
 [ "$d" == "0" ] && exit
-for v in $(seq "$val" "$d" "$target" ); do
+for v in $(seq "$val" "$d" "$target") "$target"; do
   [ "$char" == "q" ] && v="$(calc -p "round($v)")"
   busctl --user -- set-property rs.wl-gammarelay / rs.wl.gammarelay "$property" "$char" "$v"
   sleep $timestep

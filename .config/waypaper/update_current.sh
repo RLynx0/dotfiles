ed03e77b289512e4ff100e006c610ba6ee6a0fc5
RLynx0 <luc.signin96@gmail.com>
2025-02-21 18:19:04 +0100

    Update to wallpapers

---
+++ .config/waypaper/update_current.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+WAYPAPER_HOME="${XDG_CONFIG_HOME:-"$HOME/.config"}/waypaper"
+CURRENT="$WAYPAPER_HOME/current_wallpaper"
+path="$(awk -F ' *= *' -v h="$HOME" '$1 == "wallpaper" { gsub("~", h, $2); print $2 }' "$WAYPAPER_HOME/config.ini")"
+[ -f "$CURRENT" ] && rm "$CURRENT"
+ln -s "$path" "$CURRENT"

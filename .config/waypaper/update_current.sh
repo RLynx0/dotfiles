1833f4fa876cb363a6dd61a96cf3f7d7bc286faa
RLynx0 <luc.signin96@gmail.com>
2025-02-22 02:54:34 +0100

    Eh, update to the current wallpaper script

--- .config/waypaper/update_current.sh
+++ .config/waypaper/update_current.sh
@@ -3,5 +3,4 @@
 WAYPAPER_HOME="${XDG_CONFIG_HOME:-"$HOME/.config"}/waypaper"
 CURRENT="$WAYPAPER_HOME/current_wallpaper"
 path="$(awk -F ' *= *' -v h="$HOME" '$1 == "wallpaper" { gsub("~", h, $2); print $2 }' "$WAYPAPER_HOME/config.ini")"
-[ -f "$CURRENT" ] && rm "$CURRENT"
-ln -s "$path" "$CURRENT"
+ln -sf "$path" "$CURRENT"

dafd2d19fc015797b34faebabc5dd98a933647cc
RLynx0 <luc.signin96@gmail.com>
2025-10-17 14:20:44 +0200

    Update to exec-once-again.sh

--- .config/hypr/scripts/exec-once-again.sh
+++ .config/hypr/scripts/exec-once-again.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-HYPRLAND_CONFIG="${XDG_CONFIG_HOME:-"$HOME/.config"}/hypr/hyprland.conf"
+HYPRLAND_CONFIG="${XDG_CONFIG_HOME:-"$HOME/.config"}/hypr/${1:-"hyprland.conf"}"
 
 function lookup_var {
   awk -F "$1 *= *" '$2 { print $2 }' $HYPRLAND_CONFIG
@@ -19,14 +19,29 @@ function resolve {
 }
 
 function process {
-  basename="$(basename "$(awk '{ print $1 }' <<< $1)")"
-  [ ! "$(pidof $basename)" ] \
-  && printf '$ %s : ' "$1" \
-  && hyprctl dispatch exec "$1"
+  comm="$1"; inst="${2}"
+  echo "${inst/,/$'\n'}" | while read -r inst; do
+    case "$inst" in
+      'kill')
+        basename="$(basename "$(awk '{ print $1 }' <<< "$comm")")"
+        pid="$(pidof "$basename")" || continue
+        echo "Killing '$basename'" >&2
+        kill "$pid"
+        ;;
+      'exec')
+        printf "Executing '%s' : " "$comm" >&2
+        hyprctl dispatch exec "$comm" >&2
+        ;;
+      *) echo "Unknown #R instruction '$inst'" >&2 ;;
+    esac
+  done
 }
 
-exec_list="$(lookup_var "exec-once")"
+exec_list="$(lookup_var "exec-once" | awk '/\s+#R:/')"
+
 while read -r exec; do
-  process "$(resolve "$exec")"
+  command="$(echo ${exec%%#R:*})"
+  instruc="$(echo ${exec##*#R:})"
+  process "$(resolve "$command")" "$instruc"
 done <<< $exec_list
 exit 0

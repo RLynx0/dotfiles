e28f64743e72a86caf4e46b9b00a01983ba04b23
RLynx0 <luc.signin96@gmail.com>
2026-07-30 04:28:55 +0200

    Small improvements to the lua (in preparation for big improvements)

--- .config/hypr/scripts/quick-cmd.sh
+++ .config/hypr/scripts/quick-cmd.sh
@@ -91,13 +91,13 @@ source "$CTX_FILE" 2> /dev/null
 
 function setup {
   hyprctl repl "
-    function resolve_percentage(string_value, value_full)
+    local function resolve_percentage(string_value, value_full)
       local percent = string_value:match('^(%-?[%d%.]+)%%')
       if (percent) then return ((tonumber(percent) or 100) / 100) * value_full end
       return tonumber(string_value) or value_full
     end
 
-    function resolve_value(literal_value, last_literal, last_index, default, last_resolved)
+    local function resolve_value(literal_value, last_literal, last_index, default, last_resolved)
       local parts = {}
       for part in literal_value:gmatch('([^,]+)') do table.insert(parts, part) end
       local index = 1 + (last_index % #parts)
@@ -108,7 +108,7 @@ function setup {
       return { value = value or default, index = index }
     end
 
-    function resolve_dock_pos(dock_string, space, dims)
+    local function resolve_dock_pos(dock_string, space, dims)
       local x_l = space.x
       local x_r = space.x + space.width - dims.x
       local x_c = space.x + (space.width - dims.x) / 2
@@ -130,25 +130,7 @@ function setup {
       return { x = x, y = y }
     end
 
-    function setup_window(
-      workspace,
-      literal_dock,
-      literal_width,
-      literal_height,
-      default_dock,
-      default_width,
-      default_height,
-      last_dock,
-      last_width,
-      last_height,
-      last_resolved_dock,
-      last_resolved_width,
-      last_resolved_height,
-      last_dock_index,
-      last_width_index,
-      last_height_index,
-      clean_restore
-    )
+    local function setup_window(config, default, last)
       local monitor = hl.get_monitor_at_cursor()
       local reserved = monitor.reserved
       local gaps = hl.get_config('general.gaps_out')
@@ -159,51 +141,62 @@ function setup {
         width = monitor.width - reserved.left - reserved.right - gaps.left - gaps.right - 2 * border,
         height = monitor.height - reserved.top - reserved.bottom - gaps.top - gaps.bottom - 2 * border,
       }
-  
-      local resolved_dock = resolve_value(literal_dock, last_dock, last_dock_index, default_dock, last_resolved_dock)
-      local resolved_width = resolve_value(literal_width, last_width, last_width_index, default_width, last_resolved_width)
-      local resolved_height = resolve_value(literal_height, last_height, last_height_index, default_height, last_resolved_height)
+
+      local resolved_dock = resolve_value(config.dock, last.literal.dock, last.index.dock, default.dock, last.resolved.dock)
+      local resolved_width = resolve_value(config.width, last.literal.width, last.index.width, default.width, last.resolved.width)
+      local resolved_height = resolve_value(config.height, last.literal.height, last.index.height, default.height, last.resolved.height)
       local dims = {
         x = resolve_percentage(resolved_width.value, space.width),
         y = resolve_percentage(resolved_height.value, space.height),
       }
 
       local pos = resolve_dock_pos(resolved_dock.value, space, dims)
-      hl.dispatch(hl.dsp.window.move({ workspace = 'special:'..workspace }))
+      hl.dispatch(hl.dsp.window.move({ workspace = 'special:' .. config.workspace }))
       if not hl.get_active_window().floating then hl.dispatch(hl.dsp.window.float()) end
       hl.dispatch(hl.dsp.window.resize({ x = dims.x, y = dims.y, relative = false }))
       hl.dispatch(hl.dsp.window.move({ x = pos.x, y = pos.y, relative = false }))
 
-      print('LAST_RESOLVED_DOCK=\"' .. resolved_dock.value .. '\"')
-      print('LAST_RESOLVED_WIDTH=\"' .. resolved_width.value .. '\"')
-      print('LAST_RESOLVED_HEIGHT=\"' .. resolved_height.value.. '\"')
-      print('LAST_DOCK=\"' .. (clean_restore and last_dock or literal_dock) .. '\"')
-      print('LAST_WIDTH=\"' .. (clean_restore and last_width or literal_width) .. '\"')
-      print('LAST_HEIGHT=\"' .. (clean_resotre and last_height or literal_height) .. '\"')
-      print('LAST_DOCK_INDEX=\"' .. (clean_restore and last_dock_index or resolved_dock.index) .. '\"')
-      print('LAST_WIDTH_INDEX=\"' .. (clean_restore and last_width_index or resolved_width.index) .. '\"')
-      print('LAST_HEIGHT_INDEX=\"' .. (clean_restore and last_height_index or resolved_height.index).. '\"')
+      print('LAST_RESOLVED_DOCK=\"'   .. resolved_dock.value                                                   .. '\"')
+      print('LAST_RESOLVED_WIDTH=\"'  .. resolved_width.value                                                  .. '\"')
+      print('LAST_RESOLVED_HEIGHT=\"' .. resolved_height.value                                                 .. '\"')
+      print('LAST_DOCK=\"'            .. (config.clean_restore and last.literal.dock or config.dock)           .. '\"')
+      print('LAST_WIDTH=\"'           .. (config.clean_restore and last.literal.width or config.width)         .. '\"')
+      print('LAST_HEIGHT=\"'          .. (config.clean_restore and last.literal.height or config.height)       .. '\"')
+      print('LAST_DOCK_INDEX=\"'      .. (config.clean_restore and last.index.dock or resolved_dock.index)     .. '\"')
+      print('LAST_WIDTH_INDEX=\"'     .. (config.clean_restore and last.index.width or resolved_width.index)   .. '\"')
+      print('LAST_HEIGHT_INDEX=\"'    .. (config.clean_restore and last.index.height or resolved_height.index) .. '\"')
     end
 
-    setup_window(
-      '$WORKSPACE',
-      '$DOCK',
-      '$WIDTH',
-      '$HEIGHT',
-      '$DEFAULT_DOCK',
-      '$DEFAULT_WIDTH',
-      '$DEFAULT_HEIGHT',
-      '$LAST_DOCK',
-      '$LAST_WIDTH',
-      '$LAST_HEIGHT',
-      '$LAST_RESOLVED_DOCK',
-      '$LAST_RESOLVED_WIDTH',
-      '$LAST_RESOLVED_HEIGHT',
-      tonumber('$LAST_DOCK_INDEX') or 1,
-      tonumber('$LAST_WIDTH_INDEX') or 1,
-      tonumber('$LAST_HEIGHT_INDEX') or 1,
-      $CLEAN_RESTORE
-    )
+    local default = {
+      dock = '$DEFAULT_DOCK' or 'ur',
+      width = '$DEFAULT_WIDTH' or '40%',
+      height = '$DEFAULT_HEIGHT' or '100%',
+    }
+    local config = {
+      dock = '$DOCK' or default.dock,
+      width = '$WIDTH' or default.width,
+      height = '$HEIGHT' or default.height,
+      workspace = '$WORKSPACE' or 'quick--cmd',
+      clean_restore = ${CLEAN_RESTORE:-"false"},
+    }
+    local last = {
+      literal = {
+        dock = '${LAST_DOCK:-""}' or default.dock,
+        width = '${LAST_WIDTH:-""}' or default.width,
+        height = '${LAST_HEIGHT:-""}' or default.height,
+      },
+      resolved = {
+        dock = '${LAST_RESOLVED_DOCK:-""}' or default.dock,
+        width = '${LAST_RESOLVED_WIDTH:-""}' or default.width,
+        height = '${LAST_RESOLVED_HEIGHT:-""}' or default.height,
+      },
+      index = {
+        dock = tonumber('${LAST_DOCK_INDEX:-""}') or 1,
+        width = tonumber('${LAST_WIDTH_INDEX:-""}') or 1,
+        height = tonumber('${LAST_HEIGHT_INDEX:-""}') or 1,
+      },
+    }
+    setup_window(config, default, last)
   " > "$CTX_FILE"
 }
 
